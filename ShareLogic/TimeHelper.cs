c6524483f50a278a43498e270eeda48d74c7c84a
BLUEHOLE\eomtw <trim0917@gmail.com>
2023-04-26 19:06:40 +0900

    유틸리티 업데이트

---
+++ ShareLogic/TimeHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GameUtils
+{
+    public class TimeHelper
+    {
+        public const int WeekOfDay = 7;
+        public static int GetDateTimeToWeek(DateTime dateTime)
+        {
+            var days = TimeSpan.FromTicks(DateTime.UtcNow.Ticks - dateTime.Ticks).TotalDays;
+            return GetDayToWeek(days);
+        }
+        public static int GetDayToWeek(double days)
+        {
+            return (int)(days / WeekOfDay);
+        }
+    }
+}
