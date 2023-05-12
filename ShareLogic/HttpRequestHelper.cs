9763c6fcaed6d8d42461e54328dc93aa6cef29ca
Eomtaewook <trim0917@gmail.com>
2023-05-12 13:22:58 +0900

    타임아웃 추가

--- ShareLogic/HttpRequestHelper.cs
+++ ShareLogic/HttpRequestHelper.cs
@@ -1,18 +1,21 @@
 ﻿using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace GameUtils 
+namespace GameUtils
 {
     public class HttpRequestHelper
     {
         private static readonly HttpClient _webClient = new HttpClient();
 
-        public async Task<string> PostByJsonAsync(string url, string json)
+        public async Task<string> PostByJsonAsync(string url, string json, int millisecondsDelay = 5000)
         {
             var content = new StringContent(json, Encoding.UTF8, "application/json");
+            
+            var tokenSource = new CancellationTokenSource(millisecondsDelay);
 
-            var response = await _webClient.PostAsync(url, content);
+            var response = await _webClient.PostAsync(url, content, tokenSource.Token);
 
             var jsonResponse = await response.Content.ReadAsStringAsync();
 
