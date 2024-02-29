64efc861eea5941ef94f0d7329af39e973e855ca
EomTaeWook <trim0917@gmail.com>
2024-03-01 03:32:04 +0900

    비동기 헬퍼 추가

---
+++ ShareLogic/Extensions/TaskExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace GameUtils.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static T GetResult<T>(this Task<T> task, bool continueOnCapturedContext = false)
+        {
+            return task.ConfigureAwait(continueOnCapturedContext).GetAwaiter().GetResult();
+        }
+    }
+}
